c57856288d0512809010497f08dec1ac5fc8bf5f
ledka <shenghua.wang@zhangmen.com>
2018-08-22 19:08:35 +0800

    Initial Commit

---
+++ AudioConvert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioConvert
+//
+//  Created by 王胜华 on 2018/8/22.
+//  Copyright © 2018 王胜华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioConvert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioConvert
+//
+//  Created by 王胜华 on 2018/8/22.
+//  Copyright © 2018 王胜华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
