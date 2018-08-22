ba5a1726df085445d4f200f80f065cb596198c4b
ledka <shenghua.qianqing@gmail.com>
2018-08-22 19:48:07 +0800

    wav convert to aac

--- AudioConvert/ViewController.m
+++ AudioConvert/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "TPAACAudioConverter.h"
 
-@interface ViewController ()
+@interface ViewController () <TPAACAudioConverterDelegate>
+
+@property (nonatomic, strong) TPAACAudioConverter *audioConverter;
 
 @end
 
@@ -16,13 +19,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *documentsFolders = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    self.audioConverter = [[TPAACAudioConverter alloc] initWithDelegate:self
+                                                                 source:[[NSBundle mainBundle] pathForResource:@"abc" ofType:@"wav"]
+                                                            destination:[[documentsFolders objectAtIndex:0] stringByAppendingPathComponent:@"abc.m4a"]];
+    
+    NSLog(@"convert start");
+    [self.audioConverter start];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - TPAACAudioConverterDelegate
+- (void)AACAudioConverter:(TPAACAudioConverter *)converter didMakeProgress:(CGFloat)progress
+{
+//    NSLog(@"didMakeProgress:%f", progress);
+}
+
+- (void)AACAudioConverterDidFinishConversion:(TPAACAudioConverter *)converter
+{
+    NSLog(@"AACAudioConverterDidFinishConversion");
+}
+
+-(void)AACAudioConverter:(TPAACAudioConverter *)converter didFailWithError:(NSError *)error {
+    NSLog(@"didFailWithError");
+    self.audioConverter = nil;
 }
 
 
